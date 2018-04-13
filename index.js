2b8bea5f6fe0a8619b57e847d51f234f1f74d425
Tony Gorez <gorez.tony@gmail.com>
2018-04-13 18:17:07 +0200

    redux exemples

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import express from 'express'; 
+import express from 'express';
+import { createStore, combineReducers } from 'redux';
+
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -8,3 +11,53 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
+
+// STORE
+const rootReducer = combineReducers({
+    todos: todosReducer,
+    filter: filterReducer
+})
+const store = createStore(rootReducer);
+
+// ACTIONS
+const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  }
+};
+
+const setFilter = (filter) => {
+  return {
+    type: 'SET_FILTER',
+    filter
+  }
+};
+
+// REDUCERS
+function todosReducer(state = [], action) {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [...state, action.text]
+      break;
+    default:
+      return state
+  }
+}
+
+function filterReducer(state = '', action) {
+  switch(action.type) {
+    case 'SET_FILTER':
+      return action.filter
+      break;
+    default:
+      return state
+  }
+}
+
+
+// DISPATCH
+store.dispatch(addTodo('apprendre redux'));
+console.log(store.getState());
+store.dispatch(setFilter('tomate'));
+console.log(store.getState());
